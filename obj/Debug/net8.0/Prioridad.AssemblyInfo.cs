f55c51889bef5d085a79272d9820bb826cc9a88a
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-01-21 15:43:47 -0400

    creando la base de datos

--- obj/Debug/net8.0/Prioridad.AssemblyInfo.cs
+++ obj/Debug/net8.0/Prioridad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prioridad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31af6edeeb96c06f76117ccbb188dec3467bb195")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4fd8e7e99574b4e10eb3eba190d6807c049f8c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prioridad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prioridad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
