a242f0b73d342d0a0f3fc40dd60ba400bd850da2
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-01-21 18:55:22 -0400

    agregando el render mode al registro

--- obj/Debug/net8.0/Prioridad.AssemblyInfo.cs
+++ obj/Debug/net8.0/Prioridad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prioridad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d438f25b4f35c639b3c44d71fbccdf92b23c846d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+458ee5d93fb9d2df6fc805d0b0bc3c4f8d288898")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prioridad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prioridad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
