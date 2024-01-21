421f8d9ea22d1f1587138545d0f8745e1d51808e
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-01-21 16:18:25 -0400

    inyectando bll y creando el registro de clientes

--- obj/Debug/net8.0/Prioridad.AssemblyInfo.cs
+++ obj/Debug/net8.0/Prioridad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prioridad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4fd8e7e99574b4e10eb3eba190d6807c049f8c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3220ff43f58f3a3332f0e26a4ab3fade2439fb68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prioridad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prioridad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
